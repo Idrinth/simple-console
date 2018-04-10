9a8ea05aa21cc4e234d3bdb6b6b571e17c739453
Björn Büttner <Idrinth@users.noreply.github.com>
2018-04-10 21:53:26 +0200

    fixing name

---
+++ test/InputTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace De\Idrinth\Test\SimpleConsole;
+
+use De\Idrinth\SimpleConsole\Implementation\Input;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class InputTest extends TestCase
+{
+    /**
+     * @return array
+     */
+    public function provideNoneExisting()
+    {
+        return array(
+            array(new Input(array('q' => true)), 'a')
+        );
+    }
+
+    /**
+     * @dataProvider provideNodeExisting
+     * @expectedException InvalidArgumentException
+     * @param Input $input
+     * @param string $name
+     */
+    public function testNoneExisting(Input $input, $name)
+    {
+        $this->assertFalse($input->has($name));
+        $input->get($name);
+    }
+
+    /**
+     * @return array
+     */
+    public function provideExisting()
+    {
+        return array(
+            array(new Input(array('q' => true)), 'q', true)
+        );
+    }
+
+    /**
+     * @dataProvider provideExisting
+     * @param Input $input
+     * @param string $name
+     */
+    public function testExisting(Input $input, $name, $result)
+    {
+        $this->assertTrue($input->has($name));
+        $this->assertEquals($result, $input->get($name));
+    }
+}
