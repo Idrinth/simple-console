5c3cf73d8a2d27db4dd6a400388083a9d5bef7f2
Björn Büttner <Idrinth@users.noreply.github.com>
2018-04-10 22:12:23 +0200

    adding output test

---
+++ test/OutputTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace De\Idrinth\Test\SimpleConsole;
+
+use De\Idrinth\SimpleConsole\Implementation\Output;
+use PHPUnit\Framework\TestCase;
+
+class OutputTest extends TestCase
+{
+    /**
+     * @return array
+     */
+    public function provideText()
+    {
+        return array(
+            array(new Output(), "Hi, I'm text."),
+            array(new Output(), "Well, so am I."),
+        );
+    }
+
+    /**
+     * @dataProvider provideText
+     * @param Output $output
+     * @param string $text
+     */
+    public function testInfo(Output $output, $text)
+    {
+        ob_start();
+        $output->info($text);
+        $this->assertEquals("[37m[2m[3m".$text."[0m", ob_get_clean());
+    }
+
+    /**
+     * @dataProvider provideText
+     * @param Output $output
+     * @param string $text
+     */
+    public function testError(Output $output, $text)
+    {
+        ob_start();
+        $output->info($text);
+        $this->assertEquals("[31m[1m".$text."[0m", ob_get_clean());
+    }
+
+    /**
+     * @dataProvider provideText
+     * @param Output $output
+     * @param string $text
+     */
+    public function testWarning(Output $output, $text)
+    {
+        ob_start();
+        $output->info($text);
+        $this->assertEquals("[33m".$text."[0m", ob_get_clean());
+    }
+
+    /**
+     * @dataProvider provideText
+     * @param Output $output
+     * @param string $text
+     */
+    public function testSuccess(Output $output, $text)
+    {
+        ob_start();
+        $output->info($text);
+        $this->assertEquals("[32m[1m".$text."[0m", ob_get_clean());
+    }
+}
